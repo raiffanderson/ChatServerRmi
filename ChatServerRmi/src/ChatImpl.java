77e78f04acb645015a0ef366745c56482a43ed97
Raiff <Raiff@DESKTOP-F6JCBQB>
2018-03-02 01:49:27 -0300

    Melhorias.

--- ChatServerRmi/src/ChatImpl.java
+++ ChatServerRmi/src/ChatImpl.java
@@ -1,43 +1,42 @@
 import java.rmi.*;
 import java.rmi.server.*;
 import java.util.ArrayList;
-
-public class ChatImpl extends UnicastRemoteObject implements Chat {
-
+ 
+public class ChatImpl extends UnicastRemoteObject implements Chat  {
+ 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
 	private String name;
-	private Chat client = null;
+	private Chat client=null;
 	private ArrayList<String> mensagens;
-
-	public ChatImpl(String n) throws RemoteException {
+ 
+	public ChatImpl(String n)  throws RemoteException { 
 		super();
-		this.name = n;
+		this.name=n;   
 		this.mensagens = new ArrayList<String>();
 	}
-
 	public String getName() throws RemoteException {
 		return this.name;
 	}
-
-	public void setClient(Chat c) {
-		client = c;
+ 
+	public void setClient(Chat c){
+		client=c;
 	}
-
-	public Chat getClient() {
+ 
+	public Chat getClient(){
 		return client;
 	}
-
-	public void send(String s) throws RemoteException {
+ 
+	public void send(String s) throws RemoteException{
 		mensagens.add(s);
 	}
-
+	@Override
 	public ArrayList<String> lerMensagem() throws RemoteException {
 		return mensagens;
 	}
-
+	@Override
 	public void enviarMensagem(String mensagem) throws RemoteException {
 		mensagens.add(mensagem);
 	}
--- ChatServerRmi/src/ChatServer.java
+++ ChatServerRmi/src/ChatServer.java
@@ -2,26 +2,26 @@ import java.rmi.*;
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
 import java.util.*;
- 
-public class ChatServer{
-public static void main (String[] argv) {
-    try {
-    	
-	    	Scanner s=new Scanner(System.in);
-	    	System.out.print("Grupo:");
-	    	String name=s.nextLine().trim();
- 
-	    	ChatImpl server = new ChatImpl(name);	
- 
-	    	Registry registry = LocateRegistry.createRegistry(2020);
-//	    	registry.bind("rmi://localhost:1098/ServerChat", server);
-	    	Naming.rebind("rmi://localhost:2020/ServerChat", server);
 
-	    	
-	    	System.out.println("SERVIDOR ONLINE!");
- 
-    	}catch (Exception e) {
-    		System.out.println("FALHA NO SERVIDOR: " + e);
-    	}
+public class ChatServer {
+	public static void main(String[] argv) {
+		try {
+
+			Scanner s = new Scanner(System.in);
+			System.out.print("Grupo:");
+			String name = s.nextLine().trim();
+
+			ChatImpl server = new ChatImpl(name);
+
+			String bind = "rmi://localhost:2020/ServerChat";
+			Registry registry = LocateRegistry.createRegistry(2020);
+			Naming.rebind(bind, server);
+
+			System.out.println("\nSERVIDOR " + name + " ONLINE!");
+			System.out.println("Acesso em: " + bind);
+
+		} catch (Exception e) {
+			System.out.println("FALHA NO SERVIDOR: " + e);
+		}
 	}
 }
